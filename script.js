1b4e06f67a8eca2f1cfbf5657c4f1c74d00f223d
Jaum1204 <joaopedromarujo@outlook.com>
2025-01-29 22:46:08 -0300

    finished game!

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let HumanScore = 0;
-let ComputerScore = 0;
-
 function getComputerChoice(){
     let choice = Math.random();
+    if(choice < 0.33){
+        choice = "rock"
+    } else if (choice < 0.66){
+        choice = "paper"
+    } else{
+        choice = "scissors"
+    }
     return choice
 }
 
@@ -11,4 +15,50 @@ function getHumanChoice(){
     return choice;
 }
 
-console.log(getHumanChoice())
+function PlayRound(HumanChoice, CompChoice){
+    HumanChoice = HumanChoice.toLowerCase()
+
+    if(HumanChoice === CompChoice){
+        return "Draw";
+    } else if (
+        (HumanChoice === "rock" && CompChoice === "scissors") ||
+        (HumanChoice === "paper" && CompChoice === "rock") ||
+        (HumanChoice === "scissors" && CompChoice === "paper")
+    ){
+            return true;
+    } else{
+        return false;
+    }
+}
+
+function PlayGame(){
+    let PlayerScore = 0;
+    let CompScore = 0;
+
+    for (let i = 0; i < 5; i ++){
+        let HumanChoice = getHumanChoice();
+        let CompChoice = getComputerChoice();
+
+        console.log(`You chose ${HumanChoice}\nComputer chose ${CompChoice}`)
+
+        if (PlayRound(HumanChoice,CompChoice) === true){
+            PlayerScore++;
+            console.log(`Player wins this round!\nTotal score:\nPlayer: ${PlayerScore}\nComputer: ${CompScore}`)
+        }else if (PlayRound(HumanChoice,CompChoice) === false){
+            CompScore++;
+            console.log(`Computer wins this round!\nTotal score:\nPlayer: ${PlayerScore}\nComputer: ${CompScore}`)
+        }else{
+            console.log(`Draw!\nTotal score:\nPlayer: ${PlayerScore}\nComputer: ${CompScore}`)
+        }
+    }
+    console.log("END OF GAME")
+    if (PlayerScore > CompScore){
+        console.log("Player Won!")
+    } else if (CompScore > PlayerScore){
+        console.log("Computer Won!")
+    }else{
+        console.log("Draw")
+    }
+}
+
+PlayGame();
